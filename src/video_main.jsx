024145ea82746cef017c289e42fd602ed4542f6f
neil <neil.wu@yahoo.ca>
2026-07-08 16:54:15 -0400

    ref

--- src/video_main.jsx
+++ src/video_main.jsx
@@ -2,30 +2,32 @@ import { useState } from 'react';
 import VideoCreator from './components/video/video_creator';
 import AudioList from './components/video/audiolist';
 import VideoList from './components/video/videolist';
+import styles from "./components/video_main.module.css"
+
 export default function VideoMain() {
   // Track which component to show
   const [currentView, setCurrentView] = useState('home');
 
   return (
     <div>
-      <div>
+      <div className={styles.video_buttons}>
         {/* Buttons that act like links */}
         <button 
           onClick={() => setCurrentView('home')} 
-          style={{ background: 'none', border: 'none', color: 'white', cursor: 'pointer', textDecoration: 'underline' }}
+          className="link-button"
         >
           Create Video
         </button>
         
         <button 
           onClick={() => setCurrentView('audiolist')} 
-          style={{ background: 'none', border: 'none', color: 'white', cursor: 'pointer', textDecoration: 'underline' }}
+          className="link-button"
         >
           Audio List
         </button>
         <button 
           onClick={() => setCurrentView('videolist')} 
-          style={{ background: 'none', border: 'none', color: 'white', cursor: 'pointer', textDecoration: 'underline' }}
+          className="link-button"
         >
           Video List
         </button>
